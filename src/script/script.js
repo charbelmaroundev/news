5886cfcc1596e4a2ea75c3ff6dd8af19735b3ebc
CharbelMarounDev <charbelmaroun.dev@gmail.com>
2022-09-23 16:15:12 +0300

    add loop

--- src/script/script.js
+++ src/script/script.js
@@ -2,5 +2,7 @@ const articlesEl = document.getElementById('articles')
 const articles = [];
 
 $.getJSON("http://localhost/news/articles.php", data => {
-
+  for (let i = 0; i < data.length; i++) {
+  
+      }
     });
